f9e63577513f49c8b5bb47e483ce771e677f536d
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-05-08 02:08:21 +0530

    Footer-added

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="bg-gray-100 py-4 text-center text-sm text-gray-500">
+        &copy; {new Date().getFullYear()} Homie Finder. All rights reserved.
+      </footer>
+    );
+}
+
+export default Footer;
